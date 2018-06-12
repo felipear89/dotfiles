c6d24a50e1a6886d50ed4e477d943b65ee42fd8e
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2018-06-12 13:56:45 -0300

    Add .zshenv

--- oh-my-zsh/.oh-my-zsh/custom/exports.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/exports.zsh
@@ -5,20 +5,18 @@ export HISTSIZE=5000
 
 # Load Node Version Manager
 export NVM_DIR="$HOME/.nvm"
-. "/usr/local/opt/nvm/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export JAVA_HOME="$HOME/.sdkman/candidates/java/current/bin/java"
 
 export EDITOR="vim"
 
 # export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
+# eval "$(rbenv init -)"
 
-# Load pyenv
-eval "$(pyenv init -)"
-
-export GOPATH=$HOME/go
-export PATH=$PATH:$GOPATH/bin
-eval "$(goenv init -)"
+# export GOPATH=$HOME/go
+# export PATH=$PATH:$GOPATH/bin
+# eval "$(goenv init -)"
 
 source "$HOME/.vim/bundle/gruvbox/gruvbox_256palette.sh"
