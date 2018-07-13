ab56a481047e799952a42eb0ad1d80debd5382ee
Felipe Alexandre Rodrigues <felipear89@gmail.com>
2018-07-13 17:54:13 -0300

    Update alias

--- oh-my-zsh/.oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/.oh-my-zsh/custom/alias.zsh
@@ -16,3 +16,7 @@ alias diskspace_report="df -P -kHl"
 
 alias brew_update="brew -v update; brew upgrade --force-bottle --cleanup; brew cleanup; brew cask cleanup; brew prune; brew doctor; npm-check -g -u"
 
+alias mvnSonarLocal='mvn clean jacoco:prepare-agent package -U jacoco:report sonar:sonar -Dsonar.host.url="http://127.0.0.1:9001" -Dsonar.jdbc.url="jdbc:h2:tcp://127.0.0.1/sonar"'
+alias mvnSonarMutationLocal='mvn clean jacoco:prepare-agent install -U org.pitest:pitest-maven:mutationCoverage jacoco:report sonar:sonar -Dsonar.host.url="http://127.0.0.1:9001"'
+alias docker-sonar-up='sudo docker run -d --name sonarqubeLocalNetshoes -p 9001:9000 -p 9092:9092 arturbdr/sonarqube-pluggins:6.7.1.1-lts'
+alias mutationCov='mvn org.pitest:pitest-maven:mutationCoverage'
--- oh-my-zsh/.zshenv
+++ oh-my-zsh/.zshenv
@@ -4,3 +4,5 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
